4ad598ff78bd6c20ce90272581920479daf72a58
MUIZ UD DIN <mueezgamings@gmail.com>
2026-06-10 19:41:45 +0500

    added apis and working on fetching the data

--- backend/tools.py
+++ backend/tools.py
@@ -1,3 +1,4 @@
+import pathlib
 from tavily import TavilyClient
 from dotenv import load_dotenv
 import os
@@ -9,3 +10,24 @@ class AgentTools:
 
     def __init__(self) -> None:
         self.tavily_api = TavilyClient(api_key=os.getenv("tavily_api"))
+        self.result = []
+        self.research_outpt = pathlib.Path("../research_output")
+        
+    def search_web(self, query: str):
+        tavily_search = self.tavily_api.search(query=query, search_depth="advanced")
+
+        results = tavily_search.get("results")
+            
+        for result in results:
+            title, url, content =result["title"], result["url"], result["content"]
+            self.result.append({"title": title, "url": url, "content": content})
+
+        if not results:
+            return "No results found"
+
+        return tavily_search
+
+
+
+if __name__ == "__main__":
+    AgentTools().search_web("what is weahter in pakistan")
